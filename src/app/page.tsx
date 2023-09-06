5799f6316fa640d0013258b7cf9c5461aaac2095
Justin Yum <justinyum98@gmail.com>
2023-09-06 16:20:04 -0700

    edited margin for introduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-24 dark:bg-black dark:text-white">
       <div id="home-introduction" className="w-full max-w-4xl">
         <p className="text-xl">hi, my name is</p>
-        <h1 className="font-bold text-6xl">justin yum.</h1>
-        <p className="text-xl mt-4">nice to meet you 👋</p>
+        <h1 className="font-bold text-6xl mt-1 mb-4">justin yum.</h1>
+        <p className="text-xl">nice to meet you 👋</p>
       </div>
     </main>
   );
